bb88a01df5ade321c97ea49a769942dbdddbc20a
FinnCummins <finnkcummins@gmail.com>
2023-08-02 11:44:44 +0100

    Completed exercise 3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const notes = [
+let notes = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -43,6 +43,13 @@ app.get('/api/persons/:id', (request, response) => {
     else {
         response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
 })
 
 app.get('/info', (request, response) => {
